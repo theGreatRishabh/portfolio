e4076b6ce32a22ece105cf3efbc3e275c900ff06
theGreatRishabh <rk.rishabh.k@gmail.com>
2025-08-31 16:53:31 +0530

    feat: Add main components for portfolio website including Header, Hero, Footer, and Projects sections
    - Implemented Header component with responsive navigation and scroll functionality.
    - Created Hero component with typing effect for roles and social icons.
    - Developed Footer component with social media links.
    - Added HomePage component to structure the main layout.
    - Introduced Portfolio and Projects components to display project cards.
    - Created ProjectCard component for individual project details.
    - Implemented Services and Skills components to showcase services offered and skills possessed.
    - Added SectionTitle component for consistent section headings.
    - Defined constants for navigation items, services, skills, and projects.
    - Established types for Project, Service, and Skill for better type safety.
    - Configured Vite for React and Tailwind CSS integration.
    - Set up initial CSS imports and TypeScript configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import path from "path";
+import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
     base: "/portfolio/",
+    plugins: [react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
